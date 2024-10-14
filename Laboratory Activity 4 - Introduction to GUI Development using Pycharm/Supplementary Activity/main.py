22de2e8d9370adda038b0d8d50118be0c690a3c8
Vincent Roallos <qjgvgroallos@tip.edu.ph>
2024-10-14 10:28:23 +0800

    Create main.py

---
+++ Laboratory Activity 4 - Introduction to GUI Development using Pycharm/Supplementary Activity/main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from registration import *
+
+window = Tk()
+MyWin = MainWindow(window)
+window.geometry("400x300+760+390")
+window.title("Account Registration System")
+window.mainloop()
